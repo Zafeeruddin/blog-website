08bd89811dfc2d779f6dc62c7bb7a2ec61bc8f7c
Zafeeruddin <mohammed.xafeer@gmail.com>
2024-05-14 16:51:16 +0530

    first v

--- frontend/src/components/useAI.tsx
+++ frontend/src/components/useAI.tsx
@@ -3,7 +3,6 @@
 // const useAI=({prompt}:any)=>{
 
 //     const getResponse=async()=>{
-
 //         const headers={
 //             headers:{
 //                 'Content-Type':"application/json",
