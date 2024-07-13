0a3703743d59083e3217c583bef668437d71d8f7
Zafeeruddin <mohammed.xafeer@gmail.com>
2024-07-13 06:55:40 +0530

    corrected build

--- apps/frontend/src/service/apiUpdateNotifications.ts
+++ apps/frontend/src/service/apiUpdateNotifications.ts
@@ -4,7 +4,7 @@ export const updateNotification=async (token:string)=>{
     const headers={
         "Authorization":token,
     }
-    const response=await axios.put("https://backend.mohammed-xafeer.workers.dev/api/v1/user/getNotification",{withCredentials:true})
+    const response=await axios.put("https://backend.mohammed-xafeer.workers.dev/api/v1/user/getNotification",{withCredentials:true,headers})
     console.log("noitifcaitons updated",response.data) 
 }
 
