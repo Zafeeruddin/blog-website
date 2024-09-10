7b5b54a74fca49863679473c0b58113a23d1ada1
Zafeeruddin <mohammed.xafeer@gmail.com>
2024-09-10 14:29:59 +0300

    changed write

--- apps/frontend/src/service/apiVerifyEmial.ts
+++ apps/frontend/src/service/apiVerifyEmial.ts
@@ -3,7 +3,7 @@ import axios from "axios"
 export const verifyEmail= async(email:string)=>{
 
     try{
-        const response = await axios.post(`${import.meta.env.VITE_BACKEND_LOCAL_URL}/api/v1/user/checkEmail`,{email},{withCredentials:true})
+        const response = await axios.post(`${import.meta.env.VITE_BACKEND_PROD_URL}/api/v1/user/checkEmail`,{email},{withCredentials:true})
         console.log("Status is " + response.status)
         console.log("Check is " + response.data)
         if(response.status===200){
